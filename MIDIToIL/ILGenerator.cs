a85b52771364d193a9fdef0d11ab3db3b9aacac8
Koutako2024 <koutako2021@yahoo.co.jp>
2025-08-24 02:17:05 +0900

    style: isolated the IL generator part.

---
+++ MIDIToIL/ILGenerator.cs
@@ -0,0 +1,66 @@
+﻿using NAudio.Midi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIDIToIL
+{
+    internal class ILGenerator
+    {
+        private int lagTickToAllow;
+
+        public ILGenerator(int lagTickToAllow = 10)
+        {
+            this.lagTickToAllow = lagTickToAllow;
+        }
+
+        public void GenerateIL(MidiFile midiFile, string outputFilePath)
+        {
+            // filter note on events
+            List<NoteOnEvent> noteOnEvents = midiFile.Events[0]
+                .OfType<NoteOnEvent>()
+                .Where(e => e.Velocity > 0)
+                .ToList();
+
+            List<List<string>> groupedNotes = GroupNoteOnEvents(noteOnEvents);
+
+            WriteIL(outputFilePath, groupedNotes);
+        }
+
+        private List<List<string>> GroupNoteOnEvents(List<NoteOnEvent> noteOnEvents)
+        {
+            List<List<string>> groupedNotes = new();
+            for (int i = 0, k; i < noteOnEvents.Count; i += k)
+            {
+                List<string> group = new();
+                group.Add(noteOnEvents[i].NoteName);
+                for (k = 1; i + k < noteOnEvents.Count && IsSameGroup(noteOnEvents[i + k - 1], noteOnEvents[i + k]); k++)
+                {
+                    group.Add(noteOnEvents[i + k].NoteName);
+                }
+                groupedNotes.Add(group);
+            }
+            return groupedNotes;
+        }
+
+        private bool IsSameGroup(NoteOnEvent a, NoteOnEvent b)
+        {
+            return b.AbsoluteTime - a.AbsoluteTime < lagTickToAllow;
+        }
+
+        private static void WriteIL(string OutputFilePath, List<List<string>> groupedNotes)
+        {
+            using (StreamWriter writer = new(OutputFilePath, false, Encoding.UTF8))
+            {
+                for (int i = 0; i < groupedNotes.Count; i++)
+                {
+                    string chord = string.Join("", groupedNotes[i][..^1]);
+                    writer.WriteLine($"{chord}\t|{groupedNotes[i][^1]}");
+                }
+            }
+        }
+
+    }
+}
--- MIDIToIL/Program.cs
+++ MIDIToIL/Program.cs
@@ -7,27 +7,18 @@ namespace MIDIToIL
     {
         static void Main(string[] args)
         {
-            int lagToAllow = 10;
-
-            // load MIDI file
-            string filePath = @"C:\Users\kouta\Documents\projects\unfinished\ChordCode\MIDIToIL\お試し.mid";//GetMIDIFilePath();
+            // input
+            string filePath = @"C:\Users\kouta\Documents\projects\unfinished\ChordCode\MIDIToIL\お試し.mid";//AskMIDIFilePath();
             MidiFile midiFile = new(filePath, false);
 
-            // filter note on events
-            List<NoteOnEvent> noteOnEvents = midiFile.Events[0]
-                .OfType<NoteOnEvent>()
-                .Where(e => e.Velocity > 0)
-                .ToList();
-
-            //noteOnEvents.ForEach(e => Console.WriteLine(e.DeltaTime));
-
-            List<List<string>> groupedNotes = GroupNoteOnEventsByLagTime(noteOnEvents, lagToAllow);
-
-            // write IL to file
+            // output
             string? inputParent = Directory.GetParent(filePath)?.FullName;
             if (inputParent is null) throw new Exception("Cannot get parent directory of input file path.");
             string outputFilePath = Path.Join(inputParent, "out.il");
-            WriteIL(outputFilePath, groupedNotes);
+
+            //generate
+            ILGenerator generator = new();
+            generator.GenerateIL(midiFile, outputFilePath);
         }
 
         static string AskMIDIFilePath()
@@ -39,34 +30,6 @@ namespace MIDIToIL
                 filePath = Console.ReadLine();
             }
             return filePath;
-        }
-
-        static List<List<string>> GroupNoteOnEventsByLagTime(List<NoteOnEvent> noteOnEvents, int lagToAllow)
-        {
-            List<List<string>> groupedNotes = new();
-            for (int i = 0, k; i < noteOnEvents.Count; i += k)
-            {
-                List<string> group = new();
-                group.Add(noteOnEvents[i].NoteName);
-                for (k = 1; i + k < noteOnEvents.Count && noteOnEvents[i + k].DeltaTime < lagToAllow; k++)
-                {
-                    group.Add(noteOnEvents[i + k].NoteName);
-                }
-                groupedNotes.Add(group);
-            }
-            return groupedNotes;
-        }
-
-        static void WriteIL(string OutputFilePath, List<List<string>> groupedNotes)
-        {
-            using (StreamWriter writer = new(OutputFilePath, false, Encoding.UTF8))
-            {
-                for (int i = 0; i < groupedNotes.Count; i++)
-                {
-                    string chord = string.Join("", groupedNotes[i][..^1]);
-                    writer.WriteLine($"{chord}\t|{groupedNotes[i][^1]}");
-                }
-            }
         }
     }
 }
